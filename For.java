10aa09d65bd47c7cda157e81807ba826b9f23236
ramyapop <ramyamgcworld@gmail.com>
2023-01-30 16:54:54 +0530

    all done

---
+++ For.java
@@ -0,0 +1,14 @@
+package com.roughuse;
+
+public class For {
+
+	public static void main(String[] args) {
+		System.out.println("start");
+for (int i = 1; i<=3; i++) {
+	System.out.println(i + " ");
+}
+System.out.println();
+System.out.println("end");
+	}
+
+}
